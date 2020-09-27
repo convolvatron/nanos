71b69e0c78244fffb11a6689434ddd9b486e4a7e
eric hoffman <yuri@tenuki.org>
2020-09-27 15:46:36 -0700

    cut under the http router

--- src/http/management_http.c
+++ src/http/management_http.c
@@ -2,36 +2,42 @@
 #include <net.h>
 #include <http.h>
 
-closure_function(1, 3, void, each_request,
+// per request heap
+closure_function(3, 1, void, each_http_request,
                  heap, h,
-                 http_method, m,
+                 tuple, root,
                  buffer_handler, out,
                  value, v)
 {
+    rprintf("http: %v\n", v);
+    send_http_response(bound(out), timm("Content-Type", "text/html"),
+                       aprintf(bound(h),
+                               "<html>unibooty!</html>"));
+}
+
+closure_function(2, 1, buffer_handler, each_http_connection,
+                 heap, h,
+                 tuple, root, 
+                 buffer_handler, out)
+{
     heap h = bound(h);
-    rprintf("http: %s request via http: %v\n", http_request_methods[m], v);
-    buffer u = table_find(v, sym(relative_uri));
-    rprintf("foo: %b\n", u);
-    //        vector path = split(h, u, '/');        
-
-    send_http_response(out, timm("Content-Type", "text/html"), aprintf(h, "<html>unibooty!</html>"));
-    return;
-
+    return allocate_http_parser(h, closure(h, each_http_request, h, bound(root), out));
 }
 
 void init_management_http(heap h, tuple root) {
-    string r;
-    rprintf("init m\n");
-    if ((r = get(root, sym(management)))) {
-        u16 port = parse_int(r, 10, 0);
-        rprintf("init m %d\n", port);
+    string r = get(root, sym(management_http));
+    u64 port;
+    if (r && parse_int(r, 10, &port)) {
+        // and presumably not truncation
+        rprintf("management http %d\n", port);
         http_listener hl = allocate_http_listener(h, port);
         assert(hl != INVALID_ADDRESS);
-        http_register_uri_handler(hl, "", closure(h, each_request, h));
         
-        status s = listen_port(h, port, connection_handler_from_http_listener(hl));
+        status s = listen_port(h, port, closure(h, each_http_connection, h, root));
         if (!is_ok(s))
             halt("listen_port failed for http listener: %v\n", s);
         rprintf("http management server started on port %d\n", port);
+    } else {
+        rprintf("no m\n");
     }
 }
